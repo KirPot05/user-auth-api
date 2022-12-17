ba44604ecbc026794f7f541afeeb5e4f018587bf
Kiran <kiran@potdar.com>
2022-12-17 17:04:19 +0530

    modified default port

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL = process.env.BASE_URL;
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT || 80;
 export const DB_URL = process.env.DB_URL;
 export const ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
 export const AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
